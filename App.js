cc0c5a0f3e892531942ab85136f4f42fc0faa42e
Lisa Marks <lisajmarks@pm.me>
2022-03-09 15:28:53 -0800

    return data name and feels like

--- App.js
+++ App.js
@@ -3,6 +3,18 @@ import { StyleSheet, Text, View } from "react-native";
 import Weather from "./Components/Weather";
 
 export default function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+
+  async function load() {
+    const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/";
+    let url =
+      BASE_WEATHER_URL +
+      `weather?lat=${lat}&units=imperial&lon=${lon}&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+    const result = await response.json();
+  }
+
   return (
     <View style={styles.container}>
       <Weather />
--- Components/Weather.js
+++ Components/Weather.js
@@ -3,34 +3,40 @@ import React, { useEffect, useState } from "react";
 import { API_KEY } from "../utils/APIKeys";
 
 const Weather = () => {
-  const [weather, setWeather] = useState({});
-  const [city, setCity] = useState("");
+  const [data, setData] = useState();
 
   const getWeather = () => {
     let lat = "37.773972";
     let lon = "-122.431297";
 
     //construct the API url to call
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&units=imperial&lon=${lon}&appid=${API_KEY}`;
+    const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/";
+    let url =
+      BASE_WEATHER_URL +
+      `weather?lat=${lat}&units=imperial&lon=${lon}&appid=${API_KEY}`;
 
-    //call the API
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setWeather(data);
-      });
-  };
+    const response = await fetch(url);
+    const result = await response.json();
 
-  useEffect(() => {
-    getWeather();
-  }, []);
+    //call the API and set state
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setData(data);
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   getWeather();
+  // }, []);
 
   return (
     <View>
-      <Text>Weather</Text>
+      <Text>
+        City: {data?.name} <br />
+        Temprature: {data?.main.feels_like}
+      </Text>
     </View>
   );
 };
-
 export default Weather;
